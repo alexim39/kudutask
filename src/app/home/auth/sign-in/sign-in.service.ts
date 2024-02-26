6251f7c8ef44738e75cf66a2c09c9031bc73d75f
Alex Imenwo <aleximenwo@gmail.com>
2024-02-26 16:42:05 +0100

    updated backend url

--- src/app/home/auth/sign-in/sign-in.service.ts
+++ src/app/home/auth/sign-in/sign-in.service.ts
@@ -17,7 +17,8 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class SignInService {
-  private API_DOMAIN: string = environment.API_DOMAIN;
+  //private API_DOMAIN: string = environment.API_DOMAIN;
+  private API_DOMAIN: string = 'https://kudutaskapi-rhcjor10.b4a.run/';
 
   constructor(private http: HttpClient) { }
 
